bc3e50cc0f11b6f2b81a9d888de257959c91747f
Ravindra Kumar <115445860+ravi715950@users.noreply.github.com>
2023-01-06 17:53:32 +0530

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,19 @@
+burger= document.querySelector('.burger');
+// rightnav=document.querySelector('.rightnav')
+navbar=document.querySelector('.nav-bar ')
+navlist=document.querySelector('.navlist')
+
+burger.addEventListener('click',()=>{
+    navlist.classList.toggle('v-nav')
+    navbar.classList.toggle('h-nav')
+})
+function submit(){
+  alert('Please Fill The from')
+};
+
+ 
+
+
+
+
+   
